85b640719644cb1f8214a27e96c06cf4262a3483
arpit2425 <arpittrivedi2425@gmail.com>
2020-12-15 16:41:11 +0530

    Add trigger

--- functions/hello.js
+++ functions/hello.js
@@ -1,37 +1,100 @@
-/* Handle Hasura event trigger
- * This is what the structure of the event payload looks like
- *
- * {
- *   "event": {
- *       "op": "<op-name>",
- *       "data": {
- *         "old": <old-object>,
- *         "new": <new-object>
- *       }
- *   },
- *   "created_at": "<timestamp>",
- *   "id": "<uuid>",
- *   "trigger": {
- *       "name": "<name-of-trigger>",
- *       "id": "<event-uuid>"
- *   },
- *   "table":  {
- *       "schema": "<schema-name>",
- *       "name": "<table-name>"
- *   }
- * }
- *
- */
 
-exports.handler = (lambdaEvent, context, cb) => {
-  // Get the email of the user
-  const hasuraTriggerPayload = JSON.parse(lambdaEvent.body);
+const axios = require('axios');
+const moment=require('moment');
+const sub_package_query=`query vas_sub_packages($id: uuid){
+  vas_sub_packages(where:{id:{_eq:$id}}){
+    id
+    duration
+    services{
+      service_id
+    }
+  }
+}`
+const insert_tracker=`mutation insertTracker($objects: [vas_subscription_tracker_insert_input!]! ) {
+  insert_vas_subscription_tracker(objects: $objects) {
+    returning {
+      id
+    }
+  }
+}`
+const adminSecret = process.env.ADMIN_SECRET;
+const hgeEndpoint = process.env.HGE_ENDPOINT;
+exports.handler = (event, context, cb) => {
 
-  console.log(hasuraTriggerPayload);
-  const hasuraData = hasuraTriggerPayload.event.data.new;
+  try{
+    const { event: {op, data}, table: {name, schema} } = JSON.parse(event.body);
+  //const { event: {op, data}, table: {name, schema} } = event;
+  
+  let request;
+ 
+  if(op === 'INSERT'){
+    const qv ={id:data.new.sub_package_id};
+    let data1 = JSON.stringify({
+      query: sub_package_query,
+      variables: qv
+    });
+    
+    let config = {
+      method: 'post',
+      url: hgeEndpoint,
+      headers: { 
+        'content-type': 'application/json', 
+        'x-hasura-admin-secret': adminSecret
+      },
+      data : data1
+    };
+    
+   const response=await axios(config);
+   const res=response.data.data;
+  
+    
+ let {created_by,created_at,modified_at,modified_by,deleted,properties,id,log_remarks,start_date,end_date}=data.new;
+ const service_id=res.vas_sub_packages[0].services[0].service_id;
+ const duration=res.vas_sub_packages[0].duration;
+    let payload={
+      created_by,
+      created_at,
+      modified_at,
+      modified_by,
+      deleted,
+      properties,
+      sub_subscription_id:id,
+      log_remarks,
+      service_id
+    }
+    const freq=duration.split(":");
+    let frequency;
+    if(freq[0] == '01') frequency='M';
+    else if(freq[1] == '01') frequency='w';
+    else frequency='d';   
+    const final_payload=[];
+    while(moment(start_date).isSameOrBefore(end_date)){
+      payload.service_date=start_date;
+      final_payload.push(payload);
+      start_date=moment(start_date, "YYYY-MM-DD").add(1, frequency).format("YYYY-MM-DD");
+    }
+   
+    let insert_data = JSON.stringify({
+      query:insert_tracker,
+      variables: {objects:final_payload}
+    });
+    config.data=insert_data;
+   const track= await axios(config);
+    console.log(track.data);
+    
+   
+  }
+  else if(op=== 'UPDATE'){
+
+  }
+
 
   cb(null, {
     statusCode: 200,
     body: JSON.stringify({receivedData: hasuraData})
   });
+}
+catch(err){
+  console.log(err);
+}
 };
